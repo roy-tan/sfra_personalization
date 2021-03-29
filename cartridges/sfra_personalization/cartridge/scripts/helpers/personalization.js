1b6cbc4f39cb30551a2c82c57d762f79a2a55134
Roy Tan <roy.tan@salesforce.com>
2021-03-29 11:55:14 +0200

    Page Designer updates

--- cartridges/sfra_personalization/cartridge/scripts/helpers/personalization.js
+++ cartridges/sfra_personalization/cartridge/scripts/helpers/personalization.js
@@ -26,14 +26,11 @@ function getCategory(pid) {
  * @param {dw.web.ClickStreamEntry} click
  * @returns {dw.catalog.Category} category
  */
-function getProductCategory(click)
-{
+function getProductCategory(click) {
     var category;
-    if (click.pipelineName == "Product-Show")
-    {                             
+    if (click.pipelineName == "Product-Show") {
         groups = click.queryString.split("&");
-        if (groups[0].search("pid") > -1)
-        {
+        if (groups[0].search("pid") > -1) {
             pid = groups[0].split("=")[1];
             category = getCategory(pid);
         }
@@ -47,11 +44,10 @@ function getProductCategory(click)
  * @param {array} categoryIds - List of categories specified in site preferences (personalisation -> categoryIds)
  * @returns {dw.util.HashMap} categoryMap
  */
-function getSiteCategoryMap(categoryIds)
-{
+function getSiteCategoryMap(categoryIds) {
     var categoryMap = new HashMap();
     for (var i = 0, l = categoryIds.length; i < l; i++) {
-        categoryMap.put(categoryIds[i],0);
+        categoryMap.put(categoryIds[i], 0);
     }
     return categoryMap;
 }
@@ -63,15 +59,13 @@ function getSiteCategoryMap(categoryIds)
  * @param {dw.catalog.Category} productCategory
  * @returns {dw.util.HashMap} siteCategoryMap
  */
-function updateCount(siteCategoryMap, productCategory)
-{
+function updateCount(siteCategoryMap, productCategory) {
     var siteCategory;
     var iterator = siteCategoryMap.keySet().iterator();
     while (iterator.hasNext()) {
         categoryId = iterator.next();
         siteCategory = CatalogMgr.getCategory(categoryId);
-        if (productCategory == siteCategory || productCategory.isSubCategoryOf(siteCategory))
-        {
+        if (productCategory == siteCategory || productCategory.isSubCategoryOf(siteCategory)) {
             siteCategoryMap[categoryId] = siteCategoryMap[categoryId] + 1;
             break;
         }
@@ -87,18 +81,15 @@ function updateCount(siteCategoryMap, productCategory)
  * @param {dw.util.HashMap} siteCategoryMap
  * @returns {string} popularCategoryId
  */
-function getMostPopularCategory(categoryIds, siteCategoryMap)
-{
+function getMostPopularCategory(categoryIds, siteCategoryMap) {
     var popularCategoryId;
     count = 0;
     for (var i = 0; i < categoryIds.length; i++) {
         categoryId = categoryIds[i];
         Logger.info('categoryId: {0}, count: {1}', categoryId, siteCategoryMap[categoryId]);
-        if (count <= siteCategoryMap[categoryId])
-        {
+        if (count <= siteCategoryMap[categoryId]) {
             count = siteCategoryMap[categoryId];
-            if (count > 0)
-            {
+            if (count > 1) {
                 popularCategoryId = categoryId;
             }
         }
@@ -107,36 +98,31 @@ function getMostPopularCategory(categoryIds, siteCategoryMap)
 }
 
 function processClickStream() {
-    try
-    {
+    try {
         var cs = session.clickStream;
-        if (cs.isEnabled())
-        {
+        if (cs.isEnabled()) {
             var Site = require('dw/system/Site');
             var categoryIds = Site.current.getCustomPreferenceValue('categoryIds');
             var siteCategoryMap = getSiteCategoryMap(categoryIds);
-            var clicks = cs.getClicks();        
-            for (var i=0; i<clicks.length-1; i++)
-            {
+            var clicks = cs.getClicks();
+            for (var i = 0; i < clicks.length - 1; i++) {
                 productCategoryId = clicks.get(i);
                 var productCategory = getProductCategory(productCategoryId);
-                if (productCategory != null)
-                {
+                if (productCategory != null) {
                     siteCategoryMap = updateCount(siteCategoryMap, productCategory);
                 }
             }
             popularCategoryId = getMostPopularCategory(categoryIds, siteCategoryMap);
-            if (popularCategoryId != null)
-            {
+            if (popularCategoryId != null) {
                 session.custom.personalization = popularCategoryId;
                 Logger.info('session.custom.personalization: {0}', session.custom.personalization);
             }
         }
     } catch (e) {
-        Logger.error('sfra_personalization error: {0}',e.message);  
+        Logger.error('sfra_personalization error: {0}', e.message);
     }
 }
 
 module.exports = {
-    processClickStream : processClickStream
+    processClickStream: processClickStream
 };
